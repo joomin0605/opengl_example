deb3f6248b53c7ddb99d2a91b3679f34bd8355b8
joomin0605 <joomin0605@naver.com>
2021-04-10 22:57:17 +0900

    회전하는 여러 개의 큐브

--- src/program.h
+++ src/program.h
@@ -11,7 +11,11 @@ public:
 
     ~Program();
     uint32_t Get() const { return m_program; }
-    void Use() const;  
+    void Use() const;
+
+    void SetUniform(const std::string& name, int value) const;
+    void SetUniform(const std::string& name, const glm::mat4& value) const;
+    
 private:
     Program() {}
     bool Link(const std::vector<ShaderPtr>& shaders);
